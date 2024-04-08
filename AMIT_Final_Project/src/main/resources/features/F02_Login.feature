efa3defc4f1152f29ff6b1e9816b8bca6f8623d4
mostafamahmoud2001 <mostafamhmoud393@gmail.com>
2024-04-08 06:38:36 +0200

    write feature and step definition to login

--- AMIT_Final_Project/src/main/resources/features/F02_Login.feature
+++ AMIT_Final_Project/src/main/resources/features/F02_Login.feature
@@ -2,8 +2,9 @@ Feature:
 
   Scenario Outline:
     Given user go to login page
-    When  user login with "valid" "test@example.com" and "P@ssw0rd"
+    When  user login with "valid" <Email> and <Password>
     And   user press on login button
     Then  user login to the system successfully
     Examples:
-      |  |  |
+      |     Email      | Password |
+      |test@example.com| P@ssw0rd |
--- AMIT_Final_Project/src/test/java/org/example/stepDefs/D02_loginStepDef.java
+++ AMIT_Final_Project/src/test/java/org/example/stepDefs/D02_loginStepDef.java
@@ -1,4 +1,27 @@
 package org.example.stepDefs;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class D02_loginStepDef {
+
+    @Given("user go to login page")
+    public void userGoToLoginPage(){
+
+    }
+    @When("^user login with \"valid\" (.*) and (.*)$")
+    public void loginWithEmailAndPassword(String email , String password){
+
+    }
+    @And("user press on login button")
+    public void pressLoginButton(){
+
+    }
+    @Then("user login to the system successfully")
+    public void userLoginToSystem(){
+
+    }
+
 }
